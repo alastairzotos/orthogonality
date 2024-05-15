0f549dba8e7247a4752f313d8c9630fb7f8e4e67
Alastair Zotos <alastairzotos@gmail.com>
2024-05-16 00:12:21 +0300

    Handle create staff member conflict

--- apps/api/src/features/staff-members/staff-members.controller.ts
+++ apps/api/src/features/staff-members/staff-members.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, ConflictException, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { CreateStaffMemberDto } from "src/features/staff-members/staff-members.dtos";
 import { StaffMembersService } from "src/features/staff-members/staff-members.service";
 
@@ -17,7 +17,15 @@ export class StaffMembersController {
   async createStaffMember(
     @Body() staffMember: CreateStaffMemberDto,
   ) {
-    await this.staffMembersService.createStaffMember(staffMember);
+    try {
+      await this.staffMembersService.createStaffMember(staffMember);
+    } catch (e) {
+      if (e.constraint_name === 'email_idx') {
+        throw new ConflictException('Staff member with this email already exists');
+      } else {
+        throw e;
+      }
+    }
   }
 
   @Put(':id')
