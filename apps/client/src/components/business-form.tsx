6b3b84f043d64faee19ebc9d2d96ceb90579f0a4
Alastair Zotos <alastairzotos@gmail.com>
2024-05-17 00:18:28 +0300

    Form accessibility

--- apps/client/src/components/business-form.tsx
+++ apps/client/src/components/business-form.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Alert, Box, Button, FormLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Alert, Box, Button, FormLabel, MenuItem, Select, TextField, FormControl } from "@mui/material";
 import { CreateBusinessDto, businessSchema, businessTypes } from "@repo/types";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -35,45 +35,54 @@ export const BusinessForm: React.FC<Props> = ({ submitTitle, business, onChange,
       sx={{ display: 'flex', flexDirection: 'column', gap: 1, width: 500 }}
       onSubmit={handleSubmit(onChange)}
     >
-      <FormLabel>Business name</FormLabel>
-      <TextField
-        {...register('name')}
-        variant="outlined"
-        size="small"
-        placeholder="Business name"
-        helperText={errors.name && errors.name.message}
-        error={!!errors.name}
-      />
+      <FormControl>
+        <FormLabel htmlFor="name">Business name</FormLabel>
+        <TextField
+          id="name"
+          {...register('name')}
+          variant="outlined"
+          size="small"
+          placeholder="Business name"
+          helperText={errors.name && errors.name.message}
+          error={!!errors.name}
+        />
+      </FormControl>
 
-      <FormLabel>Business type</FormLabel>
-      <Controller
-        name="type"
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <Select
-            value={value}
-            onChange={onChange}
-          >
-            {businessTypes.map((businessType) => (
-              <MenuItem key={businessType} value={businessType}>
-                {capitaliseWord(businessType)}
-              </MenuItem>
-            ))}
-          </Select>
-        )}
-      />
+      <FormControl>
+        <FormLabel htmlFor="type">Business type</FormLabel>
+        <Controller
+          name="type"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <Select
+              id="type"
+              value={value}
+              onChange={onChange}
+            >
+              {businessTypes.map((businessType) => (
+                <MenuItem key={businessType} value={businessType}>
+                  {capitaliseWord(businessType)}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+      </FormControl>
 
-      <FormLabel>Location</FormLabel>
-      <TextField
-        {...register('location')}
-        variant="outlined"
-        size="small"
-        placeholder="Location"
-        multiline
-        rows={4}
-        helperText={errors.location && errors.location.message}
-        error={!!errors.location}
-      />
+      <FormControl>
+        <FormLabel htmlFor="location">Location</FormLabel>
+        <TextField
+          id="location"
+          {...register('location')}
+          variant="outlined"
+          size="small"
+          placeholder="Location"
+          multiline
+          rows={4}
+          helperText={errors.location && errors.location.message}
+          error={!!errors.location}
+        />
+      </FormControl>
 
       <Box sx={{ display: 'flex', gap: 1, pt: 3 }}>
         <Button
--- apps/client/src/components/staff-member-form.tsx
+++ apps/client/src/components/staff-member-form.tsx
@@ -1,6 +1,6 @@
 import { capitaliseWord } from "@/utils/misc";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Alert, Box, Button, FormLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Alert, Box, Button, FormLabel, MenuItem, Select, TextField, FormControl } from "@mui/material";
 import { CreateStaffMemberDto, staffMemberPositionTypes, staffMemberSchema } from "@repo/types";
 import { useRouter } from "next/router";
 import React from "react";
@@ -37,68 +37,83 @@ export const StaffMemberForm: React.FC<Props> = ({ submitTitle, staffMember, onC
       sx={{ display: 'flex', flexDirection: 'column', gap: 1, width: 500 }}
       onSubmit={handleSubmit(onChange)}
     >
-      <FormLabel>First name</FormLabel>
-      <TextField
-        {...register('firstName')}
-        variant="outlined"
-        size="small"
-        placeholder="First name"
-        helperText={errors.firstName && errors.firstName.message}
-        error={!!errors.firstName}
-      />
+      <FormControl>
+        <FormLabel htmlFor="firstName">First name</FormLabel>
+        <TextField
+          id="firstName"
+          {...register('firstName')}
+          variant="outlined"
+          size="small"
+          placeholder="First name"
+          helperText={errors.firstName && errors.firstName.message}
+          error={!!errors.firstName}
+        />
+      </FormControl>
 
-      <FormLabel>Last name</FormLabel>
-      <TextField
-        {...register('lastName')}
-        variant="outlined"
-        size="small"
-        placeholder="Last name"
-        helperText={errors.lastName && errors.lastName.message}
-        error={!!errors.lastName}
-      />
+      <FormControl>
+        <FormLabel htmlFor="lastName">Last name</FormLabel>
+        <TextField
+          id="lastName"
+          {...register('lastName')}
+          variant="outlined"
+          size="small"
+          placeholder="Last name"
+          helperText={errors.lastName && errors.lastName.message}
+          error={!!errors.lastName}
+        />
+      </FormControl>
 
-      <FormLabel>Email</FormLabel>
-      <TextField
-        {...register('email')}
-        variant="outlined"
-        size="small"
-        type="email"
-        placeholder="Email"
-        helperText={errors.email && errors.email.message}
-        error={!!errors.email}
-      />
+      <FormControl>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <TextField
+          id="email"
+          {...register('email')}
+          variant="outlined"
+          size="small"
+          type="email"
+          placeholder="Email"
+          helperText={errors.email && errors.email.message}
+          error={!!errors.email}
+        />
+      </FormControl>
 
-      <FormLabel>Phone number</FormLabel>
-      <Controller
-        name="phoneNumber"
-        control={control}
-        render={({ field: { onChange, value }, fieldState: { error }}) => (
-          <PhoneInput
-            inputStyle={{ width: '100%' }}
-            value={value}
-            onChange={onChange}
-            isValid={() => error?.message! || true}
-          />
-        )}
-      />
+      <FormControl>
+        <FormLabel htmlFor="phoneNumber">Phone number</FormLabel>
+        <Controller
+          name="phoneNumber"
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <PhoneInput
+              inputProps={{ id: 'phoneNumber' }}
+              inputStyle={{ width: '100%' }}
+              value={value}
+              onChange={onChange}
+              isValid={() => error?.message! || true}
+            />
+          )}
+        />
+      </FormControl>
 
-      <FormLabel>Position</FormLabel>
-      <Controller
-        name="position"
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <Select
-            value={value}
-            onChange={onChange}
-          >
-            {staffMemberPositionTypes.map((positionType) => (
-              <MenuItem key={positionType} value={positionType}>
-                {capitaliseWord(positionType)}
-              </MenuItem>
-            ))}
-          </Select>
-        )}
-      />
+      <FormControl>
+        <FormLabel htmlFor="position">Position</FormLabel>
+        <Controller
+          name="position"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <Select
+              id="position"
+              value={value}
+              onChange={onChange}
+            >
+              {staffMemberPositionTypes.map((positionType) => (
+                <MenuItem key={positionType} value={positionType}>
+                  {capitaliseWord(positionType)}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+      </FormControl>
 
       <Box sx={{ display: 'flex', gap: 1, pt: 3 }}>
         <Button
